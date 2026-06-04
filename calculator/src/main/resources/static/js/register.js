d5c53f06a040b92f44efa4458656ef5e2c20c9de
Vitor <vitorcarvalhosa12@gmail.com>
2026-06-04 08:30:37 -0300

    Finaliza o front da tela de cadastro

--- calculator/src/main/resources/static/js/register.js
+++ calculator/src/main/resources/static/js/register.js
@@ -1,10 +1,28 @@
 import { loadStates, loadCities } from './address.js'
 
+function nextStep() {
+    const container = document.getElementById('register_container');
+	const form = container.querySelector("#register_form");
+    const email = form.querySelector("#email");
+    const password = form.querySelector("#password");
+
+    if (!email.reportValidity() || !password.reportValidity()) return;
+
+    form.querySelector("#step1").style.display = 'none';
+    form.querySelector("#step2").style.display = 'flex';
+
+    container.querySelector("#register_title").innerText = 'Cadastre seus dados';
+
+    container.querySelector('#login_text').style.display = 'none';
+}
+
 document.addEventListener('DOMContentLoaded', async () => {
     const form = document.getElementById("register_form");
     const state = form.querySelector("#state");
     const city = form.querySelector("#city");
 
+	form.querySelector("#next_step_button").addEventListener('click', nextStep);
+	
     await loadStates(state);
 
     state.addEventListener("change", async () => {
